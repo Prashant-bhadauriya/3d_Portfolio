c4fc48d7708e07dd16307ea024c949e0194eed36
Prashant-bhadauriya <prashantbhadauriya033@gmail.com>
2025-04-29 23:51:34 +0530

    3d-portfolio setup and configuration completed

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <>
+      <main>
+        <div>
+          <h1>Welcome to my 3D Portfoilo</h1>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default App;
